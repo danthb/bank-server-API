c9741d9121f70acd295c9db9d527caa382af93a3
Daniel Tenezaca Bejarano <danthb@hotmail.com>
2022-01-13 14:45:27 -0500

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -4,6 +4,7 @@
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 . ~/.nvm/nvm.sh
 nvm install node
+node -e "console.log('Running Node.js ' + process.version)"
 
 #create our working directory if it doesnt exist
 DIR="/home/ec2-user/bank-app"
